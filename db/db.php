69f5272252071c06c9593c042795ef3db1fa235c
bicosteve <bicosteve4@gmail.com>
2021-09-29 10:49:01 +0300

    Added db remote config

--- db/db.php
+++ db/db.php
@@ -1,20 +1,19 @@
 <?php
 
-// require_once '../php/includes/config.php';
-
 //development connection
-//$db_host = "localhost";
-//$db_username = 'root';
-//$db_password = '';
-//$db_name = 'forum_app';
+// $db_host = "localhost";
+// $db_username = 'root';
+// $db_password = '';
+// $db_name = 'forum_app';
 
 //remote db connection
 $db_host = ' sql11.freemysqlhosting.net';
 $db_name = 'sql11440970';
 $db_username = 'sql11440970';
 $db_password = 'D6RqVCPrl6';
-//$db_port = '3306';
 $charset = 'utf8mb4';
+
+//db source
 $db_source = "mysql:host=$db_host;dbname=$db_name;charset=$charset";
 
 $options = [
@@ -24,7 +23,6 @@ $options = [
 ];
 
 try{
-  //$db = new PDO($db_source,"root","",$options);
   $db = new PDO($db_source,$db_username,$db_password,$options);
 }catch(PDOException $e){
   $error = $e->getMessage();
