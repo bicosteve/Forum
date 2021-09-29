171f41163f00c23e30348d88697739a8513dff83
bicosteve <bicosteve4@gmail.com>
2021-09-29 11:40:21 +0300

    Added remote db config

---
+++ php/includes/config.php
@@ -0,0 +1,5 @@
+<?php
+$user_email  = 'devbix@yahoo.com';
+$user_password = 'akashxmskypzkvuf';
+//$app_url = 'http://localhost/PHPPractise/24.Forum-App/php/';
+$app_url = 'http://php-forum-app.herokuapp.com/php/';
