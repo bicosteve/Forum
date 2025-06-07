f7111d6cecc10b9de067fbed77e0554298fdbdab
bsteve <bicosteve4@gmail.com>
2025-06-07 11:40:16 +0300

    added new bootstrap version

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -1,6 +1,4 @@
  </body>
-  <!-- jQuery -->
-  <script src="/public/static/bs/js/jquery.min.js"></script>
-  <!-- Bootstrap JavaScript -->
-  <script src="/public/static/bs/js/bootstrap.min.js"></script>
+  <!-- JS-->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </html>
--- public/includes/header.php
+++ public/includes/header.php
@@ -4,10 +4,10 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="icon" href="/public/static/img/discussion.png">
+  <link rel="icon" href="/public/img/discussion.png">
   <title>Forum | <?php echo $currentPage; ?></title>
-  <link rel="stylesheet" href="/public/static/bs/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="/public/static/css/styles.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="/public/css/styles.css" />
 </head>
 
 <body>
