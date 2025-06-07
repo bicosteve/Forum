ad04db17566d48bf49dc4dea9c2b414a4fee2084
bsteve <bicosteve4@gmail.com>
2025-06-07 12:03:20 +0300

    added min bootstrap

--- public/includes/header.php
+++ public/includes/header.php
@@ -6,11 +6,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="icon" href="/public/img/discussion.png">
   <title>Forum | <?php echo $currentPage; ?></title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="/public/css/bootstrap.min.css">
   <link rel="stylesheet" href="/public/css/styles.css" />
 </head>
 
 <body>
+  <div class="pageTitle">Thread Listing</div>
   <?php session_start(); ?>
   <?php if(isset($_SESSION['message'])): ?>
   <div style="margin-top: 10px; text-align:center;" class="row">
