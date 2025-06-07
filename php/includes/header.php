755c7db15133de85dfa886c32ff56736a65df145
bsteve <bicosteve4@gmail.com>
2025-06-07 07:08:47 +0300

    fixed the styling error

--- php/includes/header.php
+++ php/includes/header.php
@@ -4,10 +4,10 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="icon" href="../static/img/discussion.png">
+  <link rel="icon" href="../../static/img/discussion.png">
   <title>Forum | <?php echo $currentPage; ?></title>
-  <link rel="stylesheet" href="../static/bs/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="../static/css/styles.css" />
+  <link rel="stylesheet" href="../../static/bs/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="../../static/css/styles.css" />
 </head>
 
 <body>
