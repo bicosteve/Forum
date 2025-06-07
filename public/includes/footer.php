f7ba312aaffa8a1660cc93607e24a90eeae2f73d
bsteve <bicosteve4@gmail.com>
2025-06-07 12:52:48 +0300

    added jquery

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -1,5 +1,6 @@
  </body>
   <!-- JS-->
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include Bootstrap JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 </html>
